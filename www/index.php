95902611f1073c7ad0e0209368bd103567d075af
Alexandr <admin@dp-malish.com>
2016-05-22 23:25:06 +0300

    Initial commit

---
+++ www/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<meta charset = "utf-8" />
+	<title>Все статьи</title>
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/scripts.js"></script>
+    
+</head>
+<body>
+
+    
+</body>
+</html>
